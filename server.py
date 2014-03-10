2462724dbdd4d99f23552e7441e55af2953809d1
Ukang'a Dickson <ukanga@gmail.com>
2014-03-10 16:16:10 +0300

    UK: add hki support chatroom

--- server.py
+++ server.py
@@ -6,4 +6,8 @@ class SupportChatRoom(object):
     def index(self):
         raise cherrypy.HTTPRedirect("http://www.hipchat.com/gmAP6cMyi")
 
+    @cherrypy.expose
+    def hki(self):
+        raise cherrypy.HTTPRedirect("http://www.hipchat.com/gX82m2lSu")
+
 cherrypy.quickstart(SupportChatRoom())
